8e08d3b5ce03b348594a4aee1b00de91111c8e3d
salimzayd <zidsalim786@gmail.com>
2024-07-15 13:03:41 +0530

    update

--- app.js
+++ app.js
@@ -28,7 +28,6 @@ app.use(cors({
     origin: "https://food-fleet-clone-frontend.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
 }));
 app.options('*', cors({
     origin: "https://food-fleet-clone-frontend.vercel.app",
